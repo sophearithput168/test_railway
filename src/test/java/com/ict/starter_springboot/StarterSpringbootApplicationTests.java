09e2a345ff53f6a151f267e3b61fb6e7b7618372
Put Sophearith <sophearith266@rpitssr.edu.kh>
2025-10-01 19:30:16 +0700

    Initial commit

---
+++ src/test/java/com/ict/starter_springboot/StarterSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.starter_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
